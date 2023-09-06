ae24c98e790063d3e70e140a4e5f740f5c139723
Mario Iván Díaz Robledo <142775829+ShadowMist3107@users.noreply.github.com>
2023-09-06 01:25:42 -0600

    Add files via upload
    Actividad de la clase 05/09/2023

---
+++ Clase 050923/app3.js
@@ -0,0 +1,15 @@
+var nombre = "Señor sexo";
+var nombre = "El señor autoplacer";
+console.log(nombre);
+
+//Comentarios
+let user = "Momazos Diego";
+console.log(user);
+
+let numero1= 2;
+let numero2= 2;
+
+console.log(numero1+numero2);
+console.log(numero1-numero2);
+console.log(numero1 * numero2);
+console.log(numero1/numero2);
---
+++ Clase 050923/app4.js
@@ -0,0 +1,13 @@
+let nombreUser = ("Mario");
+let edad = (21);
+let nacionalidad = ("Mexicano");
+let comprometido = ("No")
+
+console.log("Nombre es: ");
+console.log(nombreUser);
+console.log("Edad es: ");
+console.log(edad);
+console.log("Su nacionalidad es: ");
+console.log(nacionalidad);
+console.log("Estas comprometid@?");
+console.log(comprometido);
